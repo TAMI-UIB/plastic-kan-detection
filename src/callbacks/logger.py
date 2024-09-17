a1f156dd3574335388b12d5d135952a812077e32
EloiSansUIB <e.sans@uib.cat>
2024-09-17 12:24:01 +0200

    [FIX] logger

--- src/callbacks/logger.py
+++ src/callbacks/logger.py
@@ -6,8 +6,8 @@ import pytorch_lightning as pl
 import torch
 from matplotlib import pyplot as plt
 from pytorch_lightning.callbacks import Callback
+from skimage.exposure import equalize_hist
 
-from ..utils.images import s2_to_rgb, calculate_ndvi, calculate_fdi
 # from .upload_drive import upload_drive, download_drive
 
 
@@ -100,6 +100,31 @@ class MetricLogger(Callback):
         # if os.environ['UPLOAD_FILES'] == 'True':
         #     upload_drive(file_name['validation'], pl_module.cfg.dataset.name)
 
+bands = ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B8A", "B9", "B11", "B12"]
+def calculate_fdi(scene):
+    # scene values [0,1e4]
+
+    NIR = scene[bands.index("B8")] * 1e-4
+    RED2 = scene[bands.index("B6")] * 1e-4
+    SWIR1 = scene[bands.index("B11")] * 1e-4
+
+    lambda_NIR = 832.9
+    lambda_RED = 664.8
+    lambda_SWIR1 = 1612.05
+    NIR_prime = RED2 + (SWIR1 - RED2) * 10 * (lambda_NIR - lambda_RED) / (lambda_SWIR1 - lambda_RED)
+
+    img = NIR - NIR_prime
+    return img
+
+def calculate_ndvi(scene):
+    NIR = scene[bands.index("B8")] * 1e-4
+    RED = scene[bands.index("B4")] * 1e-4
+    img = (NIR - RED) / (NIR + RED + 1e-12)
+    return img
+
+def s2_to_rgb(scene):
+    tensor = np.stack([scene[bands.index('B4')],scene[bands.index('B3')],scene[bands.index('B2')]])
+    return equalize_hist(tensor.swapaxes(0,1).swapaxes(1,2))
 
 class ImagePlotCallback(pl.Callback):
     def __init__(self, plot_interval=200):
@@ -127,11 +152,11 @@ class ImagePlotCallback(pl.Callback):
             # Vuelve al modo de entrenamiento
             pl_module.train()
 
-            # Convierta las listas a arrays numpy para facilitar la manipulación
-            gt_list = np.array(gt_list)
-            pred_list = np.array(pred_list)
+            N = min(max(masks.shape[0],2), 5)
 
-            N = min(max(gt_list.shape[0],2), 5)
+            images = images.cpu().numpy()
+            masks = masks.cpu().numpy()
+            preds = preds.cpu().numpy()
 
             # Crear un grid de imágenes
             preds_exp = np.where(np.exp(preds) > 0.5, 1, 0)
@@ -139,9 +164,14 @@ class ImagePlotCallback(pl.Callback):
 
             height = 3
             width = 3
+            print(images.shape)
+            images = np.transpose(images, (0, 2, 3, 1))
+            masks = np.transpose(masks, (0, 2, 3, 1))
+            preds = np.transpose(preds, (0, 2, 3, 1))
+            preds_exp = np.transpose(preds_exp, (0, 2, 3, 1))
+            preds_sig = np.transpose(preds_sig, (0, 2, 3, 1))
             fig, axs = plt.subplots(N, 6, figsize=(6 * width, N * height), squeeze=False)
-            for axs_row, img, mask, pred, pred_exp, pred_sig in zip(axs, images, masks, preds, preds_exp,
-                                                                          preds_sig):
+            for axs_row, img, mask, pred, pred_exp, pred_sig in zip(axs, images, masks, preds, preds_exp, preds_sig):
                 axs_row[0].imshow(s2_to_rgb(img))
                 axs_row[0].set_title("RGB")
                 axs_row[1].imshow(calculate_ndvi(img), cmap="viridis")
