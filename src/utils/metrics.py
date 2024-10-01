b9b6a76cafb48edcca609db3beb01c8a40c7b06b
ELoiSansUIB <e.sans@uib.eu>
2024-10-01 12:55:40 +0200

    [FIX] metrics

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,15 +1,14 @@
 import numpy as np
 import torch
 
-from sklearn.metrics import roc_auc_score, precision_recall_fscore_support, \
-    cohen_kappa_score, jaccard_score, accuracy_score
+from torchmetrics.functional.classification import accuracy, f1_score, auroc, jaccard_index, cohen_kappa
 
 metrics_dict = {
-    'accuracy': accuracy_score,
-    'fscore': precision_recall_fscore_support,
-    'auroc': roc_auc_score,
-    'jaccard': jaccard_score,
-    'kappa': cohen_kappa_score,
+    'accuracy': accuracy,
+    'fscore': f1_score,
+    'auroc': auroc,
+    'jaccard': jaccard_index,
+    'kappa': cohen_kappa,
 }
 
 class MetricCalculator:
@@ -22,15 +21,7 @@ class MetricCalculator:
         targets = targets.int()
         for i in range(inputs.size(0)):
             for k, v in self.metrics.items():
-                if k == 'fscore':
-                    _, _, f, _ = v(inputs[i].unsqueeze(0).cpu().detach().numpy(),
-                                   targets[i].unsqueeze(0).cpu().detach().numpy(),
-                                   zero_division=0, average="binary")
-                    self.dict[k].append(f)
-                else:
-                    print(inputs[i])
-                    self.dict[k].append(v(inputs[i].unsqueeze(0).cpu().detach().numpy(),
-                                          targets[i].unsqueeze(0).cpu().detach().numpy()))
+                self.dict[k].append(v(inputs[i].unsqueeze(0), targets[i].unsqueeze(0))).cpu().detach().numpy()
 
     def clean(self):
         self.dict = {k: [] for k in self.metrics.keys()}
